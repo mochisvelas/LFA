80ce4ee0a9d99653899a12abdf061cc45e788963
Brenner Hernández <velasquezmochis@gmail.com>
2020-02-26 19:32:17 -0600

    Finished malgorithm

--- LFAProject/Malgorithm.cs
+++ LFAProject/Malgorithm.cs
@@ -10,80 +10,130 @@ namespace LFAProject
     {
         string TokensER = "";
         string ActionsER = "";
+        Queue<string> gg = new Queue<string>("(.SETS./n.+.(.id.+.' '.*.=.' '.*.(.'.TAZo.'.interval.'.TAZf.'.|.'.tazo.'.interval.'.tazf.'.|.'.t09o.'.interval.'.t09f.'.).(./+.'.TAZo.'.interval.'.TAZf.'.|.'.tazo.'.interval.'.tazf.'.|.'.t09o.'.interval.'.t09f.'.).*)./n.).+.#".Split('.'));
         List<string> SetsTokens = "(.SETS./n.+.(.id.+.' '.*.=.' '.*.(.'.TAZo.'.interval.'.TAZf.'.|.'.tazo.'.interval.'.tazf.'.|.'.t09o.'.interval.'.t09f.'.).(./+.'.TAZo.'.interval.'.TAZf.'.|.'.tazo.'.interval.'.tazf.'.|.'.t09o.'.interval.'.t09f.'.).*)./n.).+.#".Split('.').ToList<string>();
-        List<string> TerminalSigns = new List<string> { "SETS", "id","TAZo", "TAZf", "interval", "tazo", "tazf", "T09o", "T09f", "/+", "/n", " ", "=", "#"};
-        List<string> OperatorSigns = new List<string> { "+", "(", ")", "?", "*", "|"};
+        List<string> TerminalSigns = new List<string> { "SETS", "id", "TAZo", "TAZf", "interval", "tazo", "tazf", "T09o", "T09f", "/+", "/n", " ", "=", "#" };
+        List<string> OperatorSigns = new List<string> { "+", "(", ")", "?", "*", "|" };
         Stack<string> TokenStack = new Stack<string>();
         Stack<BTreeNode> BTreeStack = new Stack<BTreeNode>();
-        BTree makeTree = new BTree();
-        BTreeNode nodeTree = new BTreeNode();
+        Dictionary<string, int> dicPrecedence= new Dictionary<string, int> { {"/", 1}, {"/", 2}, {"/", 2}, {"/", 3}, {"/", 3}, {"/", 3},
+        {"/", 4}, {"/", 5}, {"/", 5}, {"/", 6}};
         public void FillRETree() 
         {
-            foreach (string StTkn in SetsTokens)
+            CreateRETree(gg);                       
+        }
+
+        public bool HasMinorPrecedence(string Token) 
+        {
+            dicPrecedence.TryGetValue(Token, out int tokenValue);
+            dicPrecedence.TryGetValue(TokenStack.Peek(), out int opValue);
+            if (tokenValue <= opValue)
             {
-                CreateRETree(StTkn);
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
 
-        public bool CreateRETree(string Token) 
-        {            
-
-            if (TerminalSigns.Contains(Token))
+        public BTreeNode CreateRETree(Queue<string> Tokens) 
+        {
+            while (Tokens.Count() != 0)
             {
-                BTreeNode treeNode = new BTreeNode(Token);
-                BTreeStack.Push(treeNode);
-                return true;
-
-            } 
-            else if(Token == "(")
-            {
-                TokenStack.Push(Token);
-                return true;
-
-            }
-            else if (Token == ")")
-            {                
-                do
+                string Token = Tokens.Dequeue();
+                if (TerminalSigns.Contains(Token))
                 {
-                    if (TokenStack.Count() == 0)
-                    {
-                        return false;
-                    }
-                    if (BTreeStack.Count() < 2)
-                    {
-                        return false;
-                    }
-                    if (TokenStack.Peek() == "(")
-                    {
-                        return false;
-                    }
-
-                    BTreeNode temp = new BTreeNode(TokenStack.Pop());
-                    temp.right = BTreeStack.Pop();
-                    temp.left = BTreeStack.Pop();
-                    BTreeStack.Push(temp);
-                    TokenStack.Pop();
-
-                } while (TokenStack.Count != 0 && TokenStack.Peek() != "(");                                
-            }
-            else if (OperatorSigns.Contains(Token))
-            {
-                if (Token == "*" || Token == "+" || Token == "?")
-                {
-                    BTreeNode opNode = new BTreeNode(Token);
-                    if (BTreeStack.Count() == 0)
-                    {
-                        return false;
-                    }
-                    opNode.left = BTreeStack.Pop();
-                    BTreeStack.Push(opNode);
-                }
-                else if (true)
-                {
+                    BTreeNode treeNode = new BTreeNode(Token);
+                    BTreeStack.Push(treeNode);
+                    return null;
 
                 }
+                else if (Token == "(")
+                {
+                    TokenStack.Push(Token);
+                }
+                else if (Token == ")")
+                {
+                    do
+                    {
+                        if (TokenStack.Count() == 0)
+                        {
+                            return null;
+                        }
+                        if (BTreeStack.Count() < 2)
+                        {
+                            return null;
+                        }
+                        if (TokenStack.Peek() == "(")
+                        {
+                            return null;
+                        }
+
+                        BTreeNode temp = new BTreeNode(TokenStack.Pop());
+                        temp.right = BTreeStack.Pop();
+                        temp.left = BTreeStack.Pop();
+                        BTreeStack.Push(temp);
+                        TokenStack.Pop();
+
+                    } while (TokenStack.Count != 0 && TokenStack.Peek() != "(");
+                }
+                else if (OperatorSigns.Contains(Token))
+                {
+                    if (Token == "*" || Token == "+" || Token == "?")
+                    {
+                        BTreeNode opNode = new BTreeNode(Token);
+                        if (BTreeStack.Count() == 0)
+                        {
+                            return null;
+                        }
+                        opNode.left = BTreeStack.Pop();
+                        BTreeStack.Push(opNode);
+                    }
+                    else if (TokenStack.Count() != 0 && TokenStack.Peek() != "(" && HasMinorPrecedence(Token))
+                    {
+                        BTreeNode temp = new BTreeNode(TokenStack.Pop());
+                        if (BTreeStack.Count() < 2)
+                        {
+                            return null;
+                        }
+                        temp.right = BTreeStack.Pop();
+                        temp.left = BTreeStack.Pop();
+                        BTreeStack.Push(temp);
+                    }
+                    else if (Token == "|" || Token == ".")
+                    {
+                        TokenStack.Push(Token);
+                    }
+                }
+                else
+                {
+                    return null;
+                }
             }
-            return true;
+
+            while (TokenStack.Count() > 0)
+            {
+                if (TokenStack.Peek() == "(")
+                {
+                    return null;
+                }
+                if (BTreeStack.Count() < 2)
+                {
+                    return null;
+                }
+                BTreeNode temp = new BTreeNode(TokenStack.Pop());
+                temp.right = BTreeStack.Pop();
+                temp.left = BTreeStack.Pop();
+                BTreeStack.Push(temp);
+            }
+
+            if (BTreeStack.Count() != 1)
+            {
+                return null;
+            }
+
+            return BTreeStack.Pop();
         }
     }
 }
