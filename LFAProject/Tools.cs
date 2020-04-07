af95c6105ccd55f4e01ed63148878c0abe65e3f2
Brenner Hernández <velasquezmochis@gmail.com>
2020-04-06 18:21:18 -0600

    Added RemoveUnwantedChars method to Tools Class

--- LFAProject/Tools.cs
+++ LFAProject/Tools.cs
@@ -8,6 +8,36 @@ namespace LFAProject
 {
     class Tools
     {
+        public string RemoveUnwantedChars(string text)
+        {
+            int index = 0;
+            if (text != null)
+            {
+                while (index != -1)
+                {
+                    index = text.IndexOf(" ");
+                    if (index != -1)
+                        text = text.Remove(index, 1);
+                }
+                index = 0;
+                while (index != -1)
+                {
+                    index = text.IndexOf("\r");
+                    if (index != -1)
+                        text = text.Remove(index, 1);
+                }
+                index = 0;
+                while (index != -1)
+                {
+                    index = text.IndexOf("\t");
+                    if (index != -1)
+                        text = text.Remove(index, 1);
+                }
+                return text;
+            }
+            return text;
+        }
+
 
     }
 }
